11dc3b14c8d8284fabe6b8c849690209c9fca972
Maximiliano Bertacchini <mbertacchini@onapsis.com>
2013-10-22 19:32:50 -0300

    added match runner mgmt command (not working yet)

---
+++ tournament/management/commands/run_pending_matches.py
@@ -0,0 +1,34 @@
+import logging
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from tournament.models import Challenge
+from lightcycle.arena import LightCycleArena
+from lightcycle.player import Player
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    help = "Run queued matches."
+
+    def handle(self, *args, **options):
+        if len(args) > 0:
+            raise CommandError('Too many arguments.')
+
+        count = 0
+        for count, challenge in enumerate(Challenge.objects.filter(played=False).order_by('creation_date')):
+            try:
+                logger.info('Running Challenge "%s".' % challenge)
+                player1 = Player(challenge.challenger_bot.owner.username, challenge.challenger_bot.code)
+                player2 = Player(challenge.challenged_bot.owner.username, challenge.challenged_bot.code)
+                arena = LightCycleArena((player1, player2), settings.get('ARENA_WIDTH'), settings.get('ARENA_HEIGHT'))
+                result = arena.start()
+                print result
+            except Exception as ex:
+                msg = 'Error running Challenge "%s" ("%s")' % (challenge, ex)
+                logger.error(msg)
+                raise CommandError(msg)
+        print 'Finished running %d matches.' % count
