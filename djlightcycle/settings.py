eb561634a229fc5fafd4de84b2c16915559d559e
Maximiliano Bertacchini <maxiberta@gmail.com>
2013-10-24 04:04:33 -0300

    updated settings

--- djlightcycle/settings.py
+++ djlightcycle/settings.py
@@ -63,8 +63,6 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, '..', 'data')
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-PRIVATE_DIR = os.path.join(PROJECT_PATH, 'private')
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
