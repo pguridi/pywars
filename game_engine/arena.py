7fccc618caeabb9bdfaa87d3a5da0ac6d0ebb4d6
Pedro Guridi <pedro.guridi@gmail.com>
2014-11-08 12:51:59 -0300

    removed engine debug code

--- game_engine/arena.py
+++ game_engine/arena.py
@@ -201,12 +201,12 @@ class BattleGroundArena(object):
                         self.match.lost(player, u'Crashed')
                         raise GameOverException(str(e))
             except GameOverException as e:
-                print e
+                #print e
                 break
         else:  # for-else, if all rounds are over
             table = self.context.current_points()
             points = {}
-            print(table)
+            #print(table)
             for p, life in table.iteritems():
                 points[life] = points.get(life, []) + [p]
             top = max(points)
@@ -214,9 +214,7 @@ class BattleGroundArena(object):
                 self.match.draw()
             else:  # The player with more resistence wins
                 self.match.winner(points[top][0])
-        self.match.print_trace()
-        return ''
-        #return self.match.__json__()
+        return self.match.__json__()
 
     def _check_player_boundaries(self, player, new_x):
         half = self.width // 2
