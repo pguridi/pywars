76ccf8579231e84448d5a04a27b80617cc6572d6
Pedro Guridi <pedro.guridi@gmail.com>
2014-11-10 00:11:28 -0300

    added background music

--- game/static/js/pywars/Level.js
+++ game/static/js/pywars/Level.js
@@ -8,6 +8,7 @@ Level.prototype = {
 
 	preload: function() {
 	    this.game.load.image('background', 'static/assets/background2.png');
+	    this.game.load.audio('background_music', 'static/assets/background_music.mp3');
 	    game.time.advancedTiming = true;
 	},
 
@@ -15,6 +16,9 @@ Level.prototype = {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 		// add background for this level
 		this.game.add.tileSprite(0,0, 800, 600, 'background');
+		this.background_music = game.add.audio('background_music', 1, false);
+		this.background_music.volume -= 0.2;
+		this.background_music.play();
         this.fpsText = game.add.text(
             20, 50, '', { font: '16px Arial', fill: '#000' }
         );
