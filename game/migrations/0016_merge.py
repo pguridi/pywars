7ddd53b9077d1ed00560590cd93322729aa02e7b
Pedro Guridi <pguridi@onapsis.com>
2014-11-12 10:14:54 -0300

    added migration merge

---
+++ game/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('game', '0015_auto_20141112_1308'),
+        ('game', '0008_auto_20141111_2135'),
+    ]
+
+    operations = [
+    ]
