fb401863aadd77809eb4117bc0ba3478bf0967a3
Pedro Guridi <pedro.guridi@gmail.com>
2014-11-13 17:02:59 -0300

    many fixes in phaser frontend

--- game/static/js/pywars/gameover.js
+++ game/static/js/pywars/gameover.js
@@ -1,10 +1,12 @@
 var gameOver = function(game) {
     this.gameData = null;
+    this.players = null;
     }
 
 gameOver.prototype = {
-	init: function(gameData,  msg){
-        this.gameData = null;
+	init: function(players, gameData,  msg){
+        this.gameData = gameData;
+        this.players = players;
         if (msg != 'Draw') {
             this.msg = "Winner: " + msg;
         } else {
@@ -12,7 +14,7 @@ gameOver.prototype = {
         }
 	},
   	create: function(){
-  		var gameOverTitle = this.game.add.sprite(160,160,"gameover");
+  		//var gameOverTitle = this.game.add.sprite(160,80,"gameover");
         this.end_text = game.add.text(200, 200, '', { fontSize: '40px', fill: '#ffffff' });
         this.end_text.text = this.msg;
 
@@ -21,7 +23,7 @@ gameOver.prototype = {
 		playButton.anchor.setTo(0.5,0.5);
 	},
 	playTheGame: function(){
-        this.end_text.destroy();
-        this.game.state.start("TheGame", true, false, this.gameData);
+        //this.end_text.destroy();
+        this.game.state.start("TheGame", true, false, this.players);
 	}
 }
