47f3a05125fe2c2373bbddcf630e8f6038275e45
Pedro Guridi <pedro.guridi@gmail.com>
2014-11-13 13:02:56 -0300

    added play menu to phaser frontend

---
+++ game/static/js/pywars/preload.js
@@ -0,0 +1,43 @@
+var preload = function(game){
+    this.players = [];
+}
+
+preload.prototype = {
+    init: function(gameData){
+        this.gameData = gameData;
+    },
+
+	preload: function(){ 
+          var loadingBar = this.add.sprite(160,240,"loading");
+          loadingBar.anchor.setTo(0.5,0.5);
+          this.load.setPreloadSprite(loadingBar);
+
+        this.game.load.image('ground', 'static/assets/platform.png');
+	    this.game.load.image('background', 'static/assets/bg_castle.png');
+	    this.game.load.audio('background_music', 'static/assets/background_music.mp3');
+
+        this.game.load.spritesheet('tank', 'static/assets/tank2.png', 74, 62);
+		this.game.load.spritesheet('explosion', 'static/assets/GrenadeExplosion.png', 50, 128);
+		this.game.load.image('bullet', 'static/assets/bullet3.png');
+		this.game.load.audio('tank_running', 'static/assets/tank_running.mp3');
+		this.game.load.audio('tank_firing', 'static/assets/tank_firing.mp3');
+		this.game.load.audio('explosion', 'static/assets/explosion.mp3');
+
+		this.game.load.image("gametitle","static/assets/gametitle.png");
+		this.game.load.image("play","static/assets/play.png");
+		this.game.load.image("gameover","static/assets/gameover.png");
+
+        for (var k in this.gameData['actions']) {
+            playerData = this.gameData['actions'][k];
+            if (playerData['action'] == 'new_player') {
+                var new_player = new Player(game, playerData['name'], playerData['position']);
+                new_player.preload();
+                this.players.push(new_player);
+            }
+        }
+
+	},
+  	create: function(){
+		this.game.state.start("GameTitle", true, false, this.players);
+	}
+}
