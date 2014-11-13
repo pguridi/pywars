10a406438adcb7be46524682e58c1cbc37dd2ddd
Pedro Guridi <pedro.guridi@gmail.com>
2014-11-13 15:30:46 -0300

    gameover changes

--- game/static/js/pywars/gameover.js
+++ game/static/js/pywars/gameover.js
@@ -5,15 +5,23 @@ var gameOver = function(game) {
 gameOver.prototype = {
 	init: function(gameData,  msg){
         this.gameData = null;
-		alert(msg);
+        if (msg != 'Draw') {
+            this.msg = "Winner: " + msg;
+        } else {
+            this.msg = msg;
+        }
 	},
   	create: function(){
   		var gameOverTitle = this.game.add.sprite(160,160,"gameover");
-		gameOverTitle.anchor.setTo(0.5,0.5);
+        this.end_text = game.add.text(200, 200, '', { fontSize: '40px', fill: '#ffffff' });
+        this.end_text.text = this.msg;
+
+		//gameOverTitle.anchor.setTo(0.5,0.5);
 		var playButton = this.game.add.button(160,320,"play",this.playTheGame,this);
 		playButton.anchor.setTo(0.5,0.5);
 	},
 	playTheGame: function(){
+        this.end_text.destroy();
         this.game.state.start("TheGame", true, false, this.gameData);
 	}
 }
