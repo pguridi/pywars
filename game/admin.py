e7f78bf8deb56bca0259fdc7d928c72f922db700
Mariano Anaya <marianoanaya@gmail.com>
2014-11-15 16:16:03 -0300

    Revert "Fix final challenge"
    This reverts commit c19e8572ee6d5bcc3b4fb14c8e626c8688a3d01d.
    
    Conflicts:
    	game/admin.py

--- game/admin.py
+++ game/admin.py
@@ -26,11 +26,19 @@ class ChallengeAdmin(admin.ModelAdmin):
     list_display = ('creation_date', 'requested_by', 'challenger_bot', 'challenged_bot', 'played', 'winner_player', 'canceled')
 
 
+class BotAdmin(admin.ModelAdmin):
+    list_display = ('creation_date', 'modification_date', 'owner', 'valid')
+
+
+class ChallengeAdmin(admin.ModelAdmin):
+    list_display = ('creation_date', 'requested_by', 'challenger_bot', 'challenged_bot', 'played', 'winner_player', 'canceled')
+
+
 class FinalChallengeAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(FinalChallengeAdmin, self).get_urls()
         my_urls = patterns('',
-                           (r'^final_challenge/$', self.final_challenge)
+            (r'^final_challenge/$', self.final_challenge)
         )
         return my_urls + urls
 
@@ -39,15 +47,12 @@ class FinalChallengeAdmin(admin.ModelAdmin):
     def final_challenge(self, request, queryset):
         profiles = UserProfile.objects.filter(user__is_superuser=False).all()
         if not queryset:
-            return HttpResponseRedirect('/admin')
+		return HttpResponseRedirect('/admin')
         final_challenge = queryset[0]
-        game_set = set()
         for up_player1, up_player2 in itertools.product(profiles, repeat=2):
-            game_id = self.get_game_id(up_player1.pk, up_player2.pk)
             if (up_player1 == up_player2
                 or not up_player1.current_bot
-                or not up_player2.current_bot
-                or game_id in game_set):
+                or not up_player2.current_bot):
                 continue
             challenge = Challenge()
             challenge.requested_by = up_player1
@@ -56,7 +61,6 @@ class FinalChallengeAdmin(admin.ModelAdmin):
             challenge.final_challenge = final_challenge
             challenge.save()
             final_challenge.challenge_set.add(challenge)
-            game_set.add(game_id)
             # dispatch the new task
             players = {up_player1.user.username: up_player1.current_bot.code,
                        up_player2.user.username: up_player2.current_bot.code,
@@ -65,11 +69,6 @@ class FinalChallengeAdmin(admin.ModelAdmin):
 
         final_challenge.save()
         return HttpResponseRedirect('/admin')
-
-    def get_game_id(self, x_id, y_id):
-        min_id, max_id = (min(x_id, y_id), max(x_id, y_id))
-        return min_id, max_id
-
 
 admin.site.register(FinalChallenge, FinalChallengeAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
