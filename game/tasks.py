3267b588a120eefb1605699aed8c028b0b76d260
Mariano Anaya <marianoanaya@gmail.com>
2014-11-17 12:59:41 -0300

    Change bots validations

--- game/tasks.py
+++ game/tasks.py
@@ -54,8 +54,8 @@ def _run_match(challengue_id, players):
         cmdargs = [PYTHON_EXE, ENGINE_NAME]
 
     cmdargs.extend(['bots/' + p + '.py' for p in players.keys()])
-    randoms = [random.choice(range(GRID_LIMIT//2)),
-               random.choice(range(GRID_LIMIT//2, GRID_LIMIT))]
+    randoms = [random.choice(range(GRID_LIMIT // 2)),
+               random.choice(range(GRID_LIMIT // 2, GRID_LIMIT))]
     cmdargs.extend(map(str, randoms))
     print 'CMDARGS: ', cmdargs
     proc = subprocess.Popen(cmdargs, cwd=match_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -67,12 +67,10 @@ def _run_match(challengue_id, players):
     challng.canceled = False
 
     try:
-        r = eval(stdo) # Muy sucio.. pero es lo que hay.. :O
+        r = eval(stdo)  # Muy sucio.. pero es lo que hay.. :O
         LOSER = 'loser'
         WINNER = 'winner'
         DRAW = 'draw'
-        RESULT = 'result'
-        ACTION = 'action'
         result = r['actions'][-1]
         winner_username = result.get(WINNER)
         loser_username = result.get(LOSER)
@@ -89,35 +87,27 @@ def _run_match(challengue_id, players):
             challng.winner_player = winner
             challng.loser_player = loser
         challng.save()
-
-    except Exception as e:
+    except Exception:
         challng.played = True
         challng.canceled = True
         challng.save()
 
+
 def _validate_bot(bot_id, bot_code):
+    """Returns a <bool> indicating if :bot_code: is valid or not.
+    Modify Bot.valid according to the result.
+    """
     from game.models import Bot
     bot = Bot.objects.get(pk=bot_id)
 
     # create temp dir and dump :bot_code: in a temp file
     match_dir = tempfile.mkdtemp()
-    bots_dir = os.path.join(match_dir, 'bots')
-    os.mkdir(bots_dir)
     tmp_bot_filename = 'testing.py'
     with open(os.path.join(match_dir, tmp_bot_filename), 'w') as f:
         bot_code = bot_code.replace('gc.', '')
         f.write(bot_code)
 
-    # dump the engine and bots file in temp dir
-    shutil.copy2(ENGINE_LOCATION, match_dir)
-    shutil.copy2(ENGINE_EXCEPS, match_dir)
-
-    if os.path.exists(PYPYSANDBOX_EXE):
-        cmdargs = [PYPYSANDBOX_EXE, '--tmp={}'.format(match_dir), ENGINE_NAME]
-    else:
-        cmdargs = [PYTHON_EXE, ENGINE_NAME]
-
-    cmdargs.extend([tmp_bot_filename, tmp_bot_filename])
+    cmdargs = [PYTHON_EXE, '-m', 'py_compile', tmp_bot_filename]
 
     print "CMD: ", cmdargs
     proc = subprocess.Popen(cmdargs,
@@ -126,12 +116,10 @@ def _validate_bot(bot_id, bot_code):
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, )
     stdout, stderr = proc.communicate()
-    valid = True
-    key = 'Traceback'
+    valid = proc.returncode == 0
+    key = 'SyntaxError'
     invalid_reason = ''
-    failed_log = stderr if key in stderr else stdout
-    if key in failed_log:
-        valid = False
+    if not valid and key in (stderr or ''):
         i = stderr.index(key)
         invalid_reason = ', '.join(stderr[i:].splitlines()[-2:])
         invalid_reason = invalid_reason.replace(ENGINE_NAME, '*****')
@@ -161,7 +149,6 @@ def validate_bot(bot_id, bot_code):
         return result
 
 
-
 def _match_has_timeouted(challengue_id):
     from game.models import Challenge
     challng = Challenge.objects.get(pk=challengue_id)
@@ -178,9 +165,8 @@ def run_match(challengue_id, players):
         _run_match(challengue_id, players)
     except SoftTimeLimitExceeded:
         _match_has_timeouted(challengue_id)
-    except Exception as e:
+    except Exception:
         challng = Challenge.objects.get(pk=challengue_id)
         challng.played = True
         challng.canceled = True
         challng.save()
-
